ced1ecdddaec5b01d063407e2c8e8c499f65bd7f
Tyler Kellen <tyler@sleekcode.net>
2016-02-26 20:35:29 -0500

    remove useless comment

--- src/app.jsx
+++ src/app.jsx
@@ -6,7 +6,6 @@ import Layout from './components/layout';
 import Home from './components/home';
 import PageNotFound from './components/page-not-found';
 
-/* Routes */
 const routes = (
   <Router history={ browserHistory }>
     <Route path="/" component={ Layout }>
