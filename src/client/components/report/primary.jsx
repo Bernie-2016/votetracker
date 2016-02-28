e4901b0450431d50130878fff437b5d22fe0e171
Anne Hawley Brett <annehawleybrett@gmail.com>
2016-02-27 19:34:40 -0500

    mock html for selecting county, location, and reports

--- src/client/components/report/primary.jsx
+++ src/client/components/report/primary.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
-var counties = ['whee', 'boom', 'yay', 'silly'];
+const counties = ['county1', 'county2', 'county3', 'county4'];
+const locations = ['loc1', 'loc2', 'loc3'];
 
 const PrimaryReport = () => (
   // select county
@@ -8,11 +9,36 @@ const PrimaryReport = () => (
   // field for total Dem ballots cast
   <div className="PrimaryReportForm">
     <form>
-    <label>Total Democratic Ballots Cast
-      <input type="number" name="ballots-cast" />
+
+
+    <label>Select your County:
+      {counties.map(county => (
+        <div key={county}> {county} </div>
+      ))}
     </label>
+
+    <label>Select your Location:
+      {locations.map(location => (
+        <div key={location}> {location} </div>
+      ))}
+    </label>
+
     </form>
   </div>
 );
 
 export default PrimaryReport;
+
+    // <label>Total Democratic Ballots Cast
+    //   <input type="number" name="ballots-cast" />
+    // </label>
+
+
+  // select county/precinct
+  // phase/report type: dropdown -- [ First Count, Second Count, Final Delegate Count ]
+  // Sanders Supporters: ####
+  // Clinton Supporters: ####
+  // O’Malley Supporters: ####
+  // Other(uncommitted) Supporters: ####
+  // (only on delegate counts)
+  // ^^^ Delegates for each type: ####
