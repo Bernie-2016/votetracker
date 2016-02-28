2b62a29733ca1ffc035dbaa371885f330bbbab34
Corey Frang <gnarf@gnarf.net>
2016-02-27 22:32:01 -0500

    Set date to tuesday

--- src/client/components/home/index.jsx
+++ src/client/components/home/index.jsx
@@ -4,7 +4,7 @@ import states from '../../data/states';
 import moment from 'moment';
 import { Link } from 'react-router';
 
-const statesFiltered = states.filter(state => state.election_day.isSame(moment().startOf('day')));
+const statesFiltered = states.filter(state => state.election_day.isSame(moment('3/1', 'M/D')));
 
 const Home = () => (
   <div>
