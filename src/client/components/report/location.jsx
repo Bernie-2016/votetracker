bc5a3555302681fa09ff4fb2933428a06603e8f4
Corey Frang <gnarf@gnarf.net>
2016-02-28 13:39:08 -0500

    lint

--- src/client/components/report/location.jsx
+++ src/client/components/report/location.jsx
@@ -31,29 +31,47 @@ export default class LocationSelect extends Component {
 
   render() {
     const locations = (this.state.locations || []);
-    const updateFilter = debounce(event => {
+
+    const updateFilter = debounce(() => {
       this.setState({ filter: document.querySelector('#location-complete').value });
     }, 250);
+
     const matches = fuzzy.filter(this.state.filter || '', locations, {
-      extract: location => `${location.pollinglocation} ${location.pollingaddress} ${location.pollingcity} ${this.props.params.state} ${location.pollingzip}`,
+      extract: location => `${location.pollinglocation} ${location.pollingaddress} ` +
+        `${location.pollingcity} ${this.props.params.state} ${location.pollingzip}`,
       pre: '§',
       post: '§',
-    }).sort((a, b) => b.score - a.score || a.original.pollinglocation.localeCompare(b.original.pollinglocation));
+    }).sort((a, b) =>
+      b.score - a.score || a.original.pollinglocation.localeCompare(b.original.pollinglocation)
+    );
 
 
-    const makeLink = match => `/report/${this.props.params.state}/${this.props.params.county}/${match.original.id}/`;
+    const makeLink = match => {
+      const { state, county } = this.props.params;
+      return `/report/${state}/${county}/${match.original.id}/`;
+    };
+
     const renderItem = match => {
-      var parts = match.string.replace('§§', '').split('§');
-      var html = (<span>{parts.map((part,index) => (
+      const parts = match.string.replace('§§', '').split('§');
+      const html = (<span>{parts.map((part, index) => (
         index % 2 ? <strong key={index}>{part}</strong> : <span key={index}>{part}</span>)
       )}</span>);
       return html;
     };
 
+    const inputEvents = {
+      onKeyUp: updateFilter,
+      onChange: updateFilter,
+      onInput: updateFilter,
+    };
+
     return (
       <div className="location-form">
         <h2>Select your Polling Location</h2>
-        <input id="location-complete" onKeyUp={updateFilter} onChange={updateFilter} onInput={updateFilter} placeholder="Filter the list" />
+        <input id="location-complete"
+          {...inputEvents}
+          placeholder="Filter the list"
+        />
         <Menu items={matches} makeLink={makeLink} renderItem={renderItem} />
       </div>
     );
