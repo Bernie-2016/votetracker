91f9e968ab79c4a68b76b684f064de1427b62718
Anne Hawley Brett <annehawleybrett@gmail.com>
2016-02-27 19:35:49 -0500

    starting components for the official research, and counties and locations

--- src/client/app.jsx
+++ src/client/app.jsx
@@ -4,6 +4,7 @@ import { Router, Route, browserHistory, IndexRoute } from 'react-router';
 
 import Layout from './components/layout';
 import Home from './components/home';
+import County from './components/county';
 import PageNotFound from './components/page-not-found';
 
 import ReportRoute from './components/report/route';
@@ -12,9 +13,11 @@ const routes = (
   <Router history={ browserHistory }>
     <Route path="/" component={ Layout }>
       <IndexRoute component={ Home } />
-      <IndexRoute { ...ReportRoute } />
-        <Route path="county" component={ County } />
-        <Route path="location" component={ Location } />
+      <Route { ...ReportRoute } >
+        <Route path=":county" component={ County } >
+          <Route path=":location" component={ Location } />
+        </Route>
+      </Route>
       <Route path="*" component={ PageNotFound } />
     </Route>
   </Router>
