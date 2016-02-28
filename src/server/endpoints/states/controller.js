8acb62d2d59a5eeed59d74b843380cee2e6ea027
Corey Frang <gnarf@gnarf.net>
2016-02-27 22:04:09 -0500

    memoize databae queries

--- src/server/endpoints/states/controller.js
+++ src/server/endpoints/states/controller.js
@@ -1,5 +1,10 @@
 import db from '../../services/db';
 
+import memoize from 'lodash.memoize';
+import Debug from 'debug';
+
+const debug = Debug('states');
+
 import { dsvFormat } from 'd3-dsv';
 import fs from 'fs';
 import path from 'path';
@@ -11,19 +16,33 @@ export function read(req, res) {
   res.send(states);
 }
 
+const countiesQuery = memoize(params => {
+  debug('Caching Counties for', params);
+  return db.query(
+    'SELECT DISTINCT upper(county) as county FROM precinct_data WHERE state_code = ${state}',
+    params
+  );
+}, params => params.state);
+
 export function counties(req, res) {
-  db.query('SELECT DISTINCT upper(county) as county FROM precinct_data WHERE state_code = $1',
-    req.params.state)
+  countiesQuery(req.params)
   .then(result => {
     res.send(result.map(row => row.county).sort());
   });
 }
 
-export function locations(req, res) {
-  db.query('SELECT DISTINCT' +
+const locationsQuery = memoize(params => {
+  debug('First Query', params);
+  return db.query(
+    'SELECT DISTINCT' +
     ' precinctID as id, pollinglocation, pollingaddress, pollingcity, state_code, pollingzip' +
     ' FROM precinct_data WHERE state_code = ${state} AND county = ${county}',
-    req.params)
+    params
+  );
+}, params => `${params.state},${params.county}`);
+
+export function locations(req, res) {
+  locationsQuery(req.params)
   .then(result => {
     res.send(result);
   });
