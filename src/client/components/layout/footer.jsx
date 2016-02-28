92cce865438c79b9db34f86a12c4e0d887846e60
Anne Hawley Brett <annehawleybrett@gmail.com>
2016-02-28 16:20:59 -0500

    taking out the billionaire class

--- src/client/components/layout/footer.jsx
+++ src/client/components/layout/footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import svg from './images/billionaires.png';
+import svg from './images/billionaires-2x.png';
 
 const Footer = () => (
   <div className="footer-offset">
@@ -7,7 +7,7 @@ const Footer = () => (
       <p className="address">Bernie 2016<br />PO Box 905<br />Burlington, VT 05402</p>
       <p className="site-title">Paid for by Bernie 2016</p>
       <span className="billionaires">
-        <img src={ svg } width="200" height="40" alt="(not the billionaires)" />
+        <img src={ svg } width="195" height="28" alt="(not the billionaires)" />
       </span>
       <p><span className="copyright">&copy; Bernie 2016</span> | <a href="https://berniesanders.com/privacy-policy">Privacy Policy</a></p>
     </footer>
