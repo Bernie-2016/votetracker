e1444a80137f429c1a7847c15d4e26b6d7006b7b
Corey Frang <gnarf@gnarf.net>
2016-02-29 01:18:47 -0500

    Move filter logic to menu

--- src/client/components/report/location.jsx
+++ src/client/components/report/location.jsx
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-import fuzzy from 'fuzzy';
 import Menu from '../simple-menu';
-import debounce from 'lodash.debounce';
 
 export default class LocationSelect extends Component {
 
@@ -29,50 +27,19 @@ export default class LocationSelect extends Component {
   render() {
     const locations = (this.state.locations || []);
 
-    const updateFilter = debounce(() => {
-      this.setState({ filter: this.filterInput.value });
-    }, 250);
-
-    const matches = fuzzy.filter(this.state.filter || '', locations, {
-      extract: location => `${location.pollinglocation} ${location.pollingaddress} ` +
-        `${location.pollingcity} ${this.props.params.state} ${location.pollingzip}`,
-      pre: '§',
-      post: '§',
-    }).sort((a, b) =>
-      b.score - a.score || a.original.pollinglocation.localeCompare(b.original.pollinglocation)
-    );
-
-    const markInput = element => {
-      if (element) this.markInput(element);
-    };
-
-    const makeLink = match => {
+    const makeLink = location => {
       const { state, county } = this.props.params;
-      return `/report/${state}/${county}/${match.original.id}/`;
+      return `/report/${state}/${county}/${location.id}/`;
     };
 
-    const renderItem = match => {
-      const parts = match.string.replace('§§', '').split('§');
-      const html = (<span>{parts.map((part, index) => (
-        index % 2 ? <strong key={index}>{part}</strong> : <span key={index}>{part}</span>)
-      )}</span>);
-      return html;
-    };
-
-    const inputEvents = {
-      onKeyUp: updateFilter,
-      onChange: updateFilter,
-      onInput: updateFilter,
-    };
+    const renderItem = location =>
+      `${location.pollinglocation} ${location.pollingaddress} ` +
+        `${location.pollingcity} ${this.props.params.state} ${location.pollingzip}`;
 
     return (
       <div className="location-form">
         <h3>Select your Polling Location</h3>
-          <input ref={markInput}
-            {...inputEvents}
-            placeholder="Enter the name of your location to find matches"
-          />
-        <Menu items={matches} makeLink={makeLink} renderItem={renderItem} />
+        <Menu filter items={locations} makeLink={makeLink} renderItem={renderItem} />
       </div>
     );
   }
--- src/client/components/simple-menu/index.jsx
+++ src/client/components/simple-menu/index.jsx
@@ -1,23 +1,80 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router';
 import './styles.styl';
+import fuzzy from 'fuzzy';
+import debounce from 'lodash.debounce';
 
-const SimpleMenu = ({ items, makeLink, renderItem }) => (
-  <ul className="menu">
-  {items.map((item, index) => (
-    <li className="menu-item" key={index}>
-      <Link className="menu-link" to={makeLink(item)}>
-        {renderItem(item)}
-      </Link>
-    </li>
-  ))}
-  </ul>
-);
+export default class SimpleMenu extends Component {
+  constructor() {
+    super();
+    this.state = {
+      q: '',
+    };
+
+    this.changeFilter = this.changeFilter.bind(this);
+    this.updateFilter = debounce(this.updateFilter.bind(this), 250);
+  }
+
+  changeFilter(event) {
+    this.filterInput = event.target;
+    this.updateFilter();
+  }
+
+  updateFilter() {
+    this.setState({ q: this.filterInput.value });
+  }
+
+  render() {
+    if (!this.props) { return <span></span>; }
+    const filter = this.props.filter;
+
+    let { items, makeLink, renderItem } = this.props;
+
+    if (filter) {
+      items = fuzzy.filter(this.state.q, items, {
+        extract: renderItem,
+        pre: '§',
+        post: '§',
+      }).sort((a, b) => b.score - a.score || a.string.localeCompare(b.string));
+      const oMakeLink = makeLink;
+      makeLink = match => oMakeLink(match.original);
+      renderItem = match => {
+        const parts = match.string.replace('§§', '').split('§');
+        const html = (<span>{parts.map((part, index) => (
+          index % 2 ? <strong key={index}>{part}</strong> : <span key={index}>{part}</span>)
+        )}</span>);
+        return html;
+      };
+    }
+
+    return (
+      <ul className="menu">
+      {filter ? (
+        <li className="filter">
+          <input onChange={this.changeFilter}
+            onKeyUp={this.changeFilter}
+            onInput={this.changeFilter}
+            placeholder="Search" type="search"
+          />
+        </li>
+      ) : ''}
+      {items.map((item, index) => (
+        <li className="menu-item" key={index}>
+          <Link className="menu-link" to={makeLink(item)}>
+            {renderItem(item)}
+          </Link>
+        </li>
+      ))}
+      </ul>
+    );
+  }
+}
 
 SimpleMenu.propTypes = {
   items: React.PropTypes.array.isRequired,
   makeLink: React.PropTypes.func.isRequired,
   renderItem: React.PropTypes.func.isRequired,
+  filter: React.PropTypes.bool,
 };
 
 export default SimpleMenu;
