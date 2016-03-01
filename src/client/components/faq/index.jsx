7a39d8e2594d2625c2f298e9539d967ef7e23d0c
Corey Frang <gnarf@gnarf.net>
2016-03-01 05:30:45 -0500

    Fixing styles for FAQ

--- src/client/components/faq/index.jsx
+++ src/client/components/faq/index.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import './styles.styl';
 
 const Faq = () => {
   const email = <a href="mailto:votetracker@berniesanders.com">votetracker@berniesanders.com</a>;
+  const phone = <a href="tel:18024480742">(802) 448-0742</a>;
   return (
     <div className="faq">
       <h5>1. They couldn’t give me Democratic turnout for the day, but only total turnout,
@@ -11,15 +13,16 @@ const Faq = () => {
           How do I know which ones to enter?</h5>
       <p>If you get them broken down by precinct, it’d be great to have each one.
           If, however, you receive the results as an aggregate for all the precincts,
-          you can select ‘all precincts’ from the drop down instead.</p>
+          you can select <strong>ALL PRECINCTS</strong> instead.</p>
       <h5>3. I don’t see my polling location in my drop down. How can I report my results?</h5>
       <p>Email them to us at {email}, or you can text or call us at
-          (802) 448-0742.
-          Include all the same information you would in a report and we’ll take care of it.</p>
+          {phone}.
+          Include all the same information you would in a report (polling location, precincts,
+          vote totals) and we’ll take care of it.</p>
       <h5>4. I ran into a problem voting my polling location that I’m concerned about.
           How do I report it?</h5>
       <p>Email {email} with “PROBLEM” in the subject line or text or call
-          (802) 448-0742 with your concerns.</p>
+          {phone} with your concerns.</p>
     </div>
   );
 };
