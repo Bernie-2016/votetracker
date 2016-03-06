f8b9a8e7f52fd69f3d4bf49c3a5f40e144b7d3c3
Corey Frang <gnarf@gnarf.net>
2016-03-05 22:22:31 -0500

    Special instructions for Maine

--- src/client/app.jsx
+++ src/client/app.jsx
@@ -9,6 +9,7 @@ import PageNotFound from './components/page-not-found';
 import Official from './components/official';
 import ThankYou from './components/thank-you';
 import Faq from './components/faq';
+import Maine from './components/report/maine';
 
 import ReportRoute from './components/report/route';
 
@@ -23,6 +24,7 @@ const routes = (
     <Router history={ browserHistory }>
         <Route path="/" component={ Layout }>
           <IndexRoute component={ Home } />
+          <Route path="report/ME" component={ Maine } />
           <Route { ...ReportRoute } />
           <Route path="official" component={ Official } />
           <Route path="thank-you" component ={ ThankYou } />
---
+++ src/client/components/report/maine.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const MaineInstructions = () => (
+  <div className="maine-instructions">
+    <h3>Maine for Bernie</h3>
+    <h4>
+    To report your caucus results, please
+    text <a href="tel:503-345-7385">503-345-7385</a> with your polling location,
+    and attendees for each candidate. Thank you so much for your support!
+    </h4>
+  </div>
+);
+
+export default MaineInstructions;
